c754d2922a789989413a38cf9fea87688c237e39
Juniarto Samsudin <juniarto_samsudin@ihpc.a-star.edu.sg>
2019-03-05 12:36:24 +0800

    1. System exit in scraper.java and zipper.java

--- src/main/java/org/umgc/umgcscraper/Scraper.java
+++ src/main/java/org/umgc/umgcscraper/Scraper.java
@@ -69,6 +69,7 @@ public class Scraper implements Daemon{
     
     
     public static void main(String[] args) throws IOException, ParseException, InterruptedException, ExecutionException {
+        try{
         if (args.length != 1){
             System.out.println("Configuration file path is not provided!");
             System.out.println("Example:  /mnt/scraper.conf");
@@ -273,9 +274,13 @@ public class Scraper implements Daemon{
                     String sts = sdf.format(timeMillis);
                     System.err.println(sts + " An error was encountered with our scraper.");
                     e.printStackTrace();
+                    System.exit(1);
                 }
         }//END WHILE
-         
+        } catch (Throwable t){
+            t.printStackTrace();
+            System.exit(1);
+        } 
         //ExecutorService HeartbeatExecutor = Executors.newFixedThreadPool(1);
         //HeartbeatExecutor.submit(new HeartBeat());
         
